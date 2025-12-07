4dbc420faae348b7f7048011b080cac32b362c78
Aditya Padwal <adityapadwal02@gmail.com>
2025-12-07 23:10:23 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+// This is a decorator that marks a class as an Angular component and provides metadata about the component.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
